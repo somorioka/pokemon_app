5d55e1552d8bbf3521d98ec6632b1501ee54f0c7
morioka <sou.kokomirai@gmail.com>
2023-08-21 16:21:52 +0900

    お気に入りモードを作成

---
+++ lib/models/poke_favorite.dart
@@ -0,0 +1,13 @@
+class Favorite {
+  final int pokeId;
+
+  Favorite({
+    required this.pokeId,
+  });
+}
+
+List<Favorite> favMock = [
+  Favorite(pokeId: 1),
+  Favorite(pokeId: 4),
+  Favorite(pokeId: 7),
+];
--- lib/poke_list.dart
+++ lib/poke_list.dart
@@ -3,6 +3,7 @@ import 'package:pokemon_app/const/pokeapi.dart';
 import 'package:pokemon_app/models/pokemon_notifier.dart';
 import 'package:provider/provider.dart';
 
+import 'models/poke_favorite.dart';
 import 'poke_listitem.dart';
 
 class PokeList extends StatefulWidget {
@@ -15,35 +16,80 @@ class PokeList extends StatefulWidget {
 }
 
 class _PokeListState extends State<PokeList> {
-  static const int more = 30;
-  int pokeCount = more;
+  static const int pageSize = 30;
+  bool isFavoriteMode = false;
+  int _currentPage = 1;
+
+  int itemCount(int page){
+    int ret = page * pageSize;
+    if (isFavoriteMode && ret > favMock.length){
+      ret = favMock.length;
+    }
+    if (ret > pokeMaxId){
+      ret = pokeMaxId;
+    }
+    return ret;
+  }
+
+  int itemId (int index) {
+    int ret = index + 1;
+    if (isFavoriteMode) {
+      ret = favMock[index].pokeId;
+    }
+    return ret;
+  }
+
+  bool isLastPage(int page){
+    if (isFavoriteMode){
+      if(_currentPage * pageSize < favMock.length){
+        return false;
+      }
+      return true;
+    }else{
+      if (_currentPage * pageSize < pokeMaxId){
+        return false;
+      }
+      return true;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Consumer<PokemonsNotifier>(
-      builder: (context, pokes, child) => ListView.builder(
-        padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
-        itemCount: pokeCount + 1,
-        itemBuilder: (context, index){
-          if (index == pokeCount){
-            return OutlinedButton(
-              onPressed: () => {
-                setState((){
-                  pokeCount = pokeCount + more;
-                  if (pokeCount > pokeMaxId){
-                    pokeCount = pokeMaxId;
-                  }
-                } 
-                )
-              }, 
-              child: const Text('more')
+    return Column(
+      children: [
+        Container(
+          alignment: Alignment.topRight,
+          height: 24,
+          child: IconButton(onPressed: () => {setState(() => isFavoriteMode = !isFavoriteMode)},
+          icon: isFavoriteMode 
+          ? const Icon(Icons.star, color: Colors.orangeAccent)
+          : const Icon(Icons.star_outline)),
+          ),
+        Expanded(
+          child: Consumer<PokemonsNotifier>(
+            builder: (context, pokes, child) => ListView.builder(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+            itemCount: itemCount(_currentPage) + 1,
+            itemBuilder: (context, index){
+              if (index == itemCount(_currentPage)){
+                return OutlinedButton(
+                  onPressed: isLastPage(_currentPage)
+                  ? null 
+                  :() => {
+                    setState(() => _currentPage++)
+                  }, 
+                  child: const Text('more')
+                  );
+              }else{
+                return PokeListItem(
+                poke: pokes.byId(itemId(index)),
               );
-          }else{
-            return PokeListItem(
-            poke: pokes.byId(index + 1),
-          );
-          }
-        }
+              }
+            }
+            ),
+              ),
         ),
+      ],
     );
   }
 }
